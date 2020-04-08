9daf2c6ca6f98c7fbe965984519f2e86a07a4a29
swed1sch <swedisch97@gmail.com>
2020-04-09 02:43:19 +0300

    DONE

--- ServiceChat.cs
+++ ServiceChat.cs
@@ -21,7 +21,7 @@ namespace wcf_chat
                 operationContext = OperationContext.Current
             };
             nextId++;
-            SendMsg(user.Name + " подключился к чату",0);
+            SendMsg(" "+user.Name + " подключился к чату",0);
             users.Add(user);
             return user.ID;
         }
@@ -32,7 +32,7 @@ namespace wcf_chat
             if (user != null)
             {
                 users.Remove(user);
-                SendMsg(user.Name + "покинул чат",0);
+                SendMsg(" "+user.Name + " покинул чат",0);
             }
         }
 
@@ -44,7 +44,7 @@ namespace wcf_chat
                 var user = users.FirstOrDefault(i => i.ID == id);
                 if (user != null)
                 {
-                    answer += " : " + user.Name + " ";
+                    answer += " : " + user.Name + " : ";
                 }
                 answer += msg;
                 item.operationContext.GetCallbackChannel<IServerChatcallback>().MsgCallback(answer);
